4e4f54d7d0805abb44cc4eb5efd9443cfda33bd1
Harald Sitter <sitter@kde.org>
2016-10-28 12:51:06 +0200

    manually force kwindowsystem into the list because it has a garbage path

--- generate.rb
+++ generate.rb
@@ -244,7 +244,7 @@ source.all_build_depends
 
 apppart = SnapcraftConfig::Part.new
 apppart.after = %w(kde-frameworks-5-dev)
-apppart.build_packages = source.all_build_depends - dev_stage
+apppart.build_packages = (source.all_build_depends - dev_stage) + ['libkf5windowsystem-dev']
 apppart.configflags = %w(
   -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
   -DCMAKE_INSTALL_PREFIX=/usr
